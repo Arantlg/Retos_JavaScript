0674fd3a49a82e9f4d27308654889683135f2b9b
Arantxa Leal <2008arantxaleal@gmail.com>
2026-07-03 13:51:32 -0500

    Delete Guia-1 directory

--- Guia-1/Operaciones/R2-validacion-acceso.js
+++
@@ -1,6 +0,0 @@
-const edad=19;
-const cuentaActiva=false;
-const admin=true;
-
-puedeEntrar= edad > 18 && cuentaActiva || admin ;
-console.log("¿Puede entrar? ", puedeEntrar)
--- Guia-1/Tipo_de_datos_y_variables/R2-Datos-mascota.js
+++
@@ -1,8 +0,0 @@
-const nombre ="Venus";
-const especie="Gato";
-const edad=2;
-const vacuna=true;
-const duenio="Arantxa";
-
-const mensaje=`Mi mascota ${nombre} es un ${especie} de ${edad} años. Vacunada: ${vacuna}. Dueño: ${duenio}`;
-console.log(mensaje);
